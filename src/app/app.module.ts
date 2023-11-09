d10f343128e9c24067e6eeea82f806af2e74fedd
David Tamayo <davidtamayo286@gmail.com>
2023-11-08 21:22:34 -0500

    Creación componentes (Estructura básica): Creación de los componentes que van a ser usados en el portafolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ContactMeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
